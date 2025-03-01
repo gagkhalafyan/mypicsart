c203b584116d5302df81535a37ae717223467bce
user <user@WIN-MAJFK4MKE8V>
2025-03-01 22:15:23 +0400

    Add project files.

---
+++ 01032025/Program.cs
@@ -0,0 +1,53 @@
+﻿public class WaterTanks
+{
+    private double capacity { get; set; }
+    private double currentLevel { get; set; }
+    
+    public WaterTanks(double capacity, double currentLevel)
+    {
+        this.capacity = capacity;
+        this.currentLevel = currentLevel;
+
+    }
+
+    public static WaterTanks operator +(WaterTanks bottle1, WaterTanks bottle2)
+    {
+        if (bottle1.currentLevel + bottle2.currentLevel < bottle1.capacity)
+        {
+            return new WaterTanks(bottle1.capacity, bottle1.currentLevel + bottle2.currentLevel);
+        }
+        else
+        {
+            return new WaterTanks(bottle1.capacity, bottle1.currentLevel);
+        }
+    }
+
+    public static WaterTanks operator -(WaterTanks bottle1,double water)
+    {
+        if (bottle1.currentLevel > water)
+        {
+            return new WaterTanks(bottle1.capacity, bottle1.currentLevel - water);
+        }
+        else
+        {
+            return new WaterTanks(bottle1.capacity, bottle1.currentLevel);
+        }
+    }
+
+    public override string ToString()
+    {
+       return $"{capacity} {currentLevel}";
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        WaterTanks bottle1 = new WaterTanks(125, 78);
+        WaterTanks bottle2 = new WaterTanks(125, 4);
+        Console.WriteLine(bottle2 + bottle1);
+        Console.WriteLine(bottle1 - 77);
+    }
+}
